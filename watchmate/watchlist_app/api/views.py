968bffc618ed219006fd484fa7b43c3d9238c7af
naveenarike <naveenarike@digital-nirvana.com>
2023-12-11 18:17:28 +0530

    Status Codes:: Added status codes in the response

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from watchlist_app.api.serializers import MovieSerializer
 from watchlist_app.models import Movie
+from rest_framework import status
+
 
 @api_view(['GET', 'POST'])
 def movie_list(request):
@@ -22,9 +24,13 @@ def movie_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({"Error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+        
         serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == 'PUT':
         movie = Movie.objects.get(pk=pk)
@@ -32,14 +38,14 @@ def movie_details(request, pk):
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response({"Record deleted"})
+        return Response(status=status.HTTP_200_OK)
 
 
 
