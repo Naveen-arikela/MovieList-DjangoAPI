150c6a43fdbf40493f86eb9c4767a8b9130977e4
naveenarike <naveenarike@digital-nirvana.com>
2023-12-11 19:32:08 +0530

    Implemented Class-Based Views

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,9 +1,72 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from watchlist_app.api.serializers import MovieSerializer
 from watchlist_app.models import Movie
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 
+
+#  ╔══════════════════════════════════╗
+# ║      ⚙ Class-Based Views ⚙        ║
+#  ╚══════════════════════════════════╝
+
+class MovieListAV(APIView):
+    
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        else:
+            return Response(serializer.errors)
+
+class MovieDetailsAV(APIView):
+    
+    def get(self, request, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({"Error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        #pass old object & new object
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        #pass old object & new object
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+
+#  ╔══════════════════════════════════╗
+# ║      ⚙ Function-Based Views ⚙     ║
+#  ╚══════════════════════════════════╝
 
 @api_view(['GET', 'POST'])
 def movie_list(request):
@@ -19,7 +82,7 @@ def movie_list(request):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.err)
+            return Response(serializer.errors)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
